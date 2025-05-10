760e177ee4de28d0620081238336b70630b180f3
belyashnikovatn <101242667+belyashnikovatn@users.noreply.github.com>
2025-05-10 16:07:14 +0500

    Refactor models to include ScheduleReward and RewardLog for user reward management

--- rewards/models.py
+++ rewards/models.py
@@ -11,3 +11,29 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class ScheduleReward(models.Model):
+    """
+    Model representing a schedule for a user.
+    """
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+    executed_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.amount} coins"
+
+
+class RewardLog(models.Model):
+    """
+    Model representing a daily reward for a user.
+    """
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+    given_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.amount} coins"
